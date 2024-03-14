7edcef0a56dc3d5df372ab6ac4ab8d8ec098f4e3
kspangdg <kspang@durkangroup.com>
2024-03-14 07:41:41 -0400

    game updates, no major change to engine

--- public/scripts/game.js
+++ public/scripts/game.js
@@ -1,8 +1,14 @@
 const menu = new EB_Menu(1024, 576, "public/assets/images/menu_background.png", 'EverythingBagel.js', 'EB_core/assets/img/bagel.png'); // Config Main Menu
-const game = new EB_Config(document.createElement("canvas"), 1024, 576, {sanity: 1, dark: false, flashlight: false, noise: false}); // Config Canvas
+const game = new EB_Config(document.createElement("canvas"), 1024, 576, {
+	sanity: 1, 
+	dark: false, 
+	flashlight: false, 
+	noise: false,
+	elevator: {x: 0, y: 0, z: 0}
+});
 const clock = new EB_Clock(20); // Start clock
 const input = new EB_Input([], true); // Init input
-const music = new EB_Audio('public/assets/audio/EB_soundtrack.mp3', 0.9, true); // Music
+const music = new EB_Audio('public/assets/audio/l1_bg.mp3', 0.3, true); // Music
 const physics = new EB_Physics(); // Init physics
 
 const cursor = new EB_Sprite({
@@ -11,8 +17,8 @@ const cursor = new EB_Sprite({
 		y: 0
 	},
 	size: {
-		w: 10,
-		h: 10
+		w: 5,
+		h: 5
 	},
 	image_src: 'public/assets/images/cursor.png',
 	collision_box: {
@@ -21,8 +27,8 @@ const cursor = new EB_Sprite({
 			x: 0,
 			y: 0
 		},
-		width: 10,
-		height: 10
+		width: 5,
+		height: 5
 	}
 });
 
@@ -67,6 +73,27 @@ const message = new EB_Text({ position: {x: 512, y: 550}, size: 16, color: 'whit
 const l1_s1_cz1 = new EB_Sprite({ position: {x: 668, y: 225}, size: {w:80, h: 90}, collision_box: { active: true, offset: {x: 0, y: 0}, width: 80, height: 90} });
 const l1_s2_cz1 = new EB_Sprite({ position: {x: 0, y: 0}, size: {w:250, h: 576}, collision_box: { active: true, offset: {x: 0, y: 0}, width: 250, height: 576} });
 
+let elevator_buttons_x = [];
+let elevator_buttons_x_position = [{x: 446, y: 293}, {x: 498, y: 293}, {x: 552, y: 293}, {x: 604, y: 293}];
+let elevator_buttons_y = [];
+let elevator_buttons_y_position = [{x: 397, y: 245}, {x: 397, y: 193}, {x: 397, y: 140}, {x: 397, y: 87}];
+let elevator_buttons_z = [];
+let elevator_buttons_z_position = [{x: 652, y: 245}, {x: 652, y: 193}, {x: 652, y: 140}, {x: 652, y: 87}];
+
+// Elevator x
+for (let index = 0; index < 4; index++) {
+	elevator_buttons_x.push(new EB_Sprite({ position: elevator_buttons_x_position[index], size: {w: 37, h: 37}, image_src: 'public/assets/images/elevator_btn.png', collision_box: { active: true, offset: {x: 0, y: 0}, width: 37, height: 37} }));
+}
+// Elevator y
+for (let index = 0; index < 4; index++) {
+	elevator_buttons_y.push(new EB_Sprite({ position: elevator_buttons_y_position[index], size: {w: 37, h: 37}, image_src: 'public/assets/images/elevator_btn.png', collision_box: { active: true, offset: {x: 0, y: 0}, width: 37, height: 37} }));
+}
+// Elevator z
+for (let index = 0; index < 4; index++) {
+	elevator_buttons_z.push(new EB_Sprite({ position: elevator_buttons_z_position[index], size: {w: 37, h: 37}, image_src: 'public/assets/images/elevator_btn.png', collision_box: { active: true, offset: {x: 0, y: 0}, width: 37, height: 37} }));
+}
+const elevator_message = new EB_Text({ position: {x: 601, y: 448}, size: 20, color: 'lightgreen', text: '{x: 0, y: 0, z: 0}' });
+const elevator_button_go = new EB_Sprite({ position: {x: 390, y: 413}, size: {w: 59, h: 59}, collision_box: { active: true, offset: {x: 0, y: 0}, width: 59, height: 59} });
 
 // Main Menu
 menu.init();
@@ -86,7 +113,6 @@ function mute() {
 
 // Start the game
 function init() {
-	game.mute = true;
 	menu.hide();
 	clock.start();
 	game.start();
--- public/scripts/update.js
+++ public/scripts/update.js
@@ -5,11 +5,8 @@ function update() {
     game.clear();
     clock.update();
     background.update();
-    noise.update();
-    if (game.meta.dark) flashlight.update();
     message.update();
     message.display = false;
-    cursor.update();
 
     // Update cursor position
     cursor.position.x = input.mouse.x;
@@ -41,8 +38,62 @@ function update() {
         if (game.scene == 2) {
             background.image.src = 'public/assets/images/level_1.1.png';
             l1_s2_cz1.update();
+            // Back to scene 1
             if (physics.collision(l1_s2_cz1, cursor).any && input.mouse.clicked) {
                 game.scene = 1;
+            }
+            /*
+             * ELEVATOR
+            */
+            // elevator display
+            elevator_message.display = true;
+            elevator_message.update();
+            elevator_message.text = '{x: ' + game.meta.elevator.x + ', y: ' + game.meta.elevator.y + ', z: ' + game.meta.elevator.z + '}';
+
+            // elevator buttons x
+            for (let index = 0; index < elevator_buttons_x.length; index++) {
+                elevator_buttons_x[index].update();
+                if (index + 1 == game.meta.elevator.x) {
+                    elevator_buttons_x[index].image.src = 'public/assets/images/elevator_btn_active.png';
+                } else {
+                    elevator_buttons_x[index].image.src = 'public/assets/images/elevator_btn.png';
+                }
+                if (physics.collision(elevator_buttons_x[index], cursor).any && input.mouse.clicked) {
+                    game.meta.elevator.x = index + 1;
+                } 
+            }
+
+            // elevator buttons y
+            for (let index = 0; index < elevator_buttons_y.length; index++) {
+                elevator_buttons_y[index].update();
+                if (index + 1 == game.meta.elevator.y) {
+                    elevator_buttons_y[index].image.src = 'public/assets/images/elevator_btn_active.png';
+                } else {
+                    elevator_buttons_y[index].image.src = 'public/assets/images/elevator_btn.png';
+                }
+                if (physics.collision(elevator_buttons_y[index], cursor).any && input.mouse.clicked) {
+                    game.meta.elevator.y = index + 1;
+                } 
+            }
+            // elevator buttons z
+            for (let index = 0; index < elevator_buttons_z.length; index++) {
+                elevator_buttons_z[index].update();
+                if (index + 1 == game.meta.elevator.z) {
+                    elevator_buttons_z[index].image.src = 'public/assets/images/elevator_btn_active.png';
+                } else {
+                    elevator_buttons_z[index].image.src = 'public/assets/images/elevator_btn.png';
+                }
+                if (physics.collision(elevator_buttons_z[index], cursor).any && input.mouse.clicked) {
+                    game.meta.elevator.z = index + 1;
+                } 
+            }
+            // elevator buttons go
+            elevator_button_go.update();
+            if (physics.collision(elevator_button_go, cursor).any && input.mouse.clicked) {
+                game.scene = 1;
+                game.meta.elevator.x = 0;
+                game.meta.elevator.y = 0;
+                game.meta.elevator.z = 0;
             }
         }
     }
@@ -51,4 +102,8 @@ function update() {
     if (game.level == 2) {
         //background.image.src = 'public/assets/images/menu_background.png';
     }
+
+    noise.update();
+    if (game.meta.dark) flashlight.update();
+    cursor.update();
 }
